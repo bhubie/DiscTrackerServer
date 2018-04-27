1730915f4679e14b407e8b6eaf25a650748ef28d
Brett Huber <bretthuber6@gmail.com>
2018-04-26 22:58:10 -0500

    enable cors for api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import * as cors from 'cors';
 
 const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+  app.use(cors());
   await app.listen(port);
-  console.log(`Started up on port ${port}`)
 }
 bootstrap();
